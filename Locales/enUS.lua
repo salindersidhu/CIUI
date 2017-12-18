a6c0600bd59f5b0028b7e90be04a9838d5d3d08d
Salinder Sidhu <salinder.sid@gmail.com>
2017-12-18 03:20:40 -0500

    Updates
    - Updated .gitignore
    - Added basic template for options menu

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -6,5 +6,7 @@ if not(GetLocale() == "enUS") then
 end
 
 -- English (DEFAULT)
+L["STRING_BUTTON_DEFAULTS"] = "Defaults"
 L["STRING_TITLE"] = "Improved Hotkey Text"
-L["STRING_OPTIONS_DESCRIPTION"] = "These options allow you to modify the apparence of hotkey text on the action bars."
+L["STRING_ADDON_DESCRIPTION"] = "This addon modifies the apparence of hotkey text on the action bars."
+L["STRING_OPTIONS_DESCRIPTION"] = "These options allows you to change the hotkey modifier prefix and colour."
--- options.lua
+++ options.lua
@@ -16,9 +16,42 @@ local function initInterfaceOptions(frame)
 	-- Title description
 	local titleDesc = optsFrame:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
 	titleDesc:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
-	titleDesc:SetText(L["STRING_OPTIONS_DESCRIPTION"])
+	titleDesc:SetText(L["STRING_ADDON_DESCRIPTION"])
 	titleDesc:SetJustifyH("LEFT")
 	titleDesc:SetWidth(592)
+
+	-- Enable checkbox
+	local chkEnable = CreateFrame("CheckButton", "chkEnable", optsFrame, "ChatConfigCheckButtonTemplate")
+	chkEnable:SetPoint("TOPLEFT", titleDesc, "BOTTOMLEFT", -2, -15)
+	_G[chkEnable:GetName().."Text"]:SetText(" Enable")
+	chkEnable:SetWidth(30)
+	chkEnable:SetHeight(30)
+	chkEnable:SetChecked(true)
+
+	-- Restore default configuration button
+	local resetButton = CreateFrame("Button", "resetButton", optsFrame, "OptionsButtonTemplate")
+	resetButton:SetPoint("TOPRIGHT", titleDesc, "BOTTOMRIGHT", 0, -15)
+	resetButton:SetText(L["STRING_BUTTON_DEFAULTS"])
+	resetButton:SetWidth(100)
+
+	-- Option description
+	local optsDesc = optsFrame:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+	optsDesc:SetPoint("TOPLEFT", chkEnable, "BOTTOMLEFT", 0, -8)
+	optsDesc:SetText(L["STRING_OPTIONS_DESCRIPTION"])
+	optsDesc:SetJustifyH("LEFT")
+	optsDesc:SetWidth(592)
+
+	--
+	local d = CreateFrame("Frame", "drop", optsFrame, "UIDropDownMenuTemplate")
+	d:SetPoint("TOPLEFT", 0, -55)
+
+	-- 
+	local editHotkey = CreateFrame("EditBox", "editHotkey", optsFrame, "InputBoxTemplate")
+	editHotkey:SetPoint("TOPLEFT", optsDesc, "BOTTOMLEFT", 5, -50)
+	editHotkey:SetSize(60, 25)
+	editHotkey:SetAutoFocus(false)
+	editHotkey:SetText("S")
+	editHotkey:SetCursorPosition(0)
 
 	-- Assign frame to interface options menu
 	InterfaceOptions_AddCategory(optsFrame)
